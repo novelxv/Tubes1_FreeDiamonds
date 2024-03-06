4d1751d739b2be2fbc860e084370ef3e753cb428
dianatrihy <dianatrihy@gmail.com>
2024-03-06 13:06:19 +0700

    masukin  bot punya lidya

--- tubes1-IF2211-bot-starter-pack-1.0.1/main.py
+++ tubes1-IF2211-bot-starter-pack-1.0.1/main.py
@@ -9,6 +9,7 @@ from game.logic.random import RandomLogic
 from game.util import *
 from game.logic.base import BaseLogic
 from game.logic.novel import Novel
+from game.logic.lidya import Lidya
 
 init()
 BASE_URL = "http://localhost:3000/api"
@@ -16,6 +17,7 @@ DEFAULT_BOARD_ID = 1
 CONTROLLERS = {
     "Random": RandomLogic,
     "Novel": Novel,
+    "Lidya": Lidya,
 }
 
 ###############################################################################
